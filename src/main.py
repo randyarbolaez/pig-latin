4878e2fac7c2f0c8675ba13d66b54489d12cecb2
randyarbolaez <rarbolaez001@gmail.com>
2020-12-29 17:09:45 -0500

    pig-latin | user input

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+def input_string_to_translate_to_pig_latin(prompt):
+    return input(prompt)
+
+
 def main():
-    print('pig-latin')
+    user_input = input_string_to_translate_to_pig_latin('What would you like to translate to pig latin? ')
+
+    print(user_input)
 
 main()
