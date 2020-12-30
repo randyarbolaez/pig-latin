f862df4224e90fe74ce31d2eb0da497f51d83532
randyarbolaez <rarbolaez001@gmail.com>
2020-12-30 16:37:42 -0500

    pig-latin | words that start with vowel add 'way' at the end completed

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
-def input_string_to_translate_to_pig_latin(prompt):
-    return input(prompt)
+VOWELS = ['a','e','i','o','u']
 
+
+def input_string_to_translate_to_pig_latin(prompt):
+    return input(prompt).strip().lower()
+
+def split_str(input_str):
+    return input_str.split()
+
+def translate_to_pig_latin(words):
+    pig_latin_str = ''
+    for word in words:
+        if word[0] in VOWELS:
+           pig_latin_str = word + 'way'
+           continue
+    print('===================', pig_latin_str)
 
 def main():
     user_input = input_string_to_translate_to_pig_latin('What would you like to translate to pig latin? ')
-
-    print(user_input)
+    list_of_words = split_str(user_input)
+    translate_to_pig_latin(list_of_words)
 
 main()
