977ab775b8664838e6e029d9e3644a347213eb2b
randyarbolaez <rarbolaez001@gmail.com>
2020-12-31 13:21:46 -0500

    pig-latin | fixed error where the string was getting reset every loop

--- src/main.py
+++ src/main.py
@@ -11,13 +11,17 @@ def translate_to_pig_latin(words):
     pig_latin_str = ''
     for word in words:
         if word[0] in VOWELS:
-           pig_latin_str = word + 'way'
-           continue
-    print('===================', pig_latin_str)
+            word_with_way = word + 'yay '
+            pig_latin_str += word_with_way
+            continue
+        else:
+            pig_latin_str += word + ' '
+    return pig_latin_str
 
 def main():
     user_input = input_string_to_translate_to_pig_latin('What would you like to translate to pig latin? ')
     list_of_words = split_str(user_input)
-    translate_to_pig_latin(list_of_words)
+    pig_latin = translate_to_pig_latin(list_of_words)
+    print(pig_latin)
 
 main()
