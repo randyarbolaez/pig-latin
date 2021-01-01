050894369be47f25a732e5f57b64594732cdeab6
randyarbolaez <rarbolaez001@gmail.com>
2021-01-01 11:21:02 -0500

    pig-latin | completed

--- src/main.py
+++ src/main.py
@@ -7,6 +7,15 @@ def input_string_to_translate_to_pig_latin(prompt):
 def split_str(input_str):
     return input_str.split()
 
+def word_does_not_begin_with_vowel(word):
+    pig_latin_word = word 
+    for letter in word:
+        if letter not in VOWELS:
+            pig_latin_word = pig_latin_word.replace(letter,'')
+            pig_latin_word += letter
+        if letter in VOWELS:
+            return pig_latin_word + 'ay '
+
 def translate_to_pig_latin(words):
     pig_latin_str = ''
     for word in words:
@@ -15,7 +24,7 @@ def translate_to_pig_latin(words):
             pig_latin_str += word_with_way
             continue
         else:
-            pig_latin_str += word + ' '
+            pig_latin_str += word_does_not_begin_with_vowel(word)
     return pig_latin_str
 
 def main():
